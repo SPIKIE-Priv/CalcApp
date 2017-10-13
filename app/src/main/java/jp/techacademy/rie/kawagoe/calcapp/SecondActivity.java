e594941a8a5fd94bd9d2466b814c3df9b602e9d1
rkawagoe <rkawagoe@1worldsync.com>
2017-10-13 14:51:09 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/rie/kawagoe/calcapp/SecondActivity.java
@@ -0,0 +1,21 @@
+package jp.techacademy.rie.kawagoe.calcapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Intent intent = getIntent();
+        double result = intent.getDoubleExtra("VALUE1",0);
+
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(String.valueOf(result));
+    }
+}
