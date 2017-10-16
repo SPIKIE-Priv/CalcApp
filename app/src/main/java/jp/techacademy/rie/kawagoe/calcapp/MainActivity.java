b60fb9b2efe6aabcb46bb7df023d0d53d21b3e2b
rkawagoe <rkawagoe@1worldsync.com>
2017-10-17 08:17:36 +0900

    課題修正2

--- app/src/main/java/jp/techacademy/rie/kawagoe/calcapp/MainActivity.java
+++ app/src/main/java/jp/techacademy/rie/kawagoe/calcapp/MainActivity.java
@@ -1,5 +1,6 @@
 package jp.techacademy.rie.kawagoe.calcapp;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.AlertDialog;
@@ -9,13 +10,14 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.text.InputType;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     EditText mEditText1;
     EditText mEditText2;
-    
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,28 +40,55 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
 
-
     @Override
     public void onClick(View v) {
-        double etext1 = Double.parseDouble(mEditText1.getText().toString());
-        double etext2 = Double.parseDouble(mEditText2.getText().toString());
+        try {
+            double etext1 = Double.parseDouble(mEditText1.getText().toString());
+            double etext2 = Double.parseDouble(mEditText2.getText().toString());
 
-        Intent intent = new Intent(this, SecondActivity.class);
+            Intent intent = new Intent(this, SecondActivity.class);
 
-            if (v.getId() == R.id.button1){
+            if (v.getId() == R.id.button1) {
                 intent.putExtra("VALUE1", etext1 + etext2);
-        }
-        else if (v.getId() == R.id.button2){
+            } else if (v.getId() == R.id.button2) {
                 intent.putExtra("VALUE1", etext1 - etext2);
-        }
-        else if (v.getId() == R.id.button3){
+            } else if (v.getId() == R.id.button3) {
                 intent.putExtra("VALUE1", etext1 * etext2);
-        }
-        else if (v.getId() == R.id.button4){
+            } else if (v.getId() == R.id.button4) {
                 intent.putExtra("VALUE1", etext1 / etext2);
-        }
+            }
 
-        startActivity(intent);
+            startActivity(intent);
+        } catch (
+                NumberFormatException e)
+
+        {
+            showAlertDialog();
+        }
+    }
+
+
+    private void showAlertDialog(){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        // Setting Dialog Title
+        alertDialogBuilder.setTitle("Alert Dialog");
+
+        // Setting Dialog Message
+        alertDialogBuilder.setMessage("数値が入力されていません");
+
+
+        // Setting OK Button
+        alertDialogBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener(){
+            @Override
+            public void onClick(DialogInterface dialog, int which){
+                mEditText1.getEditableText().clear();
+                mEditText2.getEditableText().clear();
+                mEditText1.requestFocus();
+            }
+        });
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        // Showing Alert Message
+        alertDialog.show();
     }
 }
 
